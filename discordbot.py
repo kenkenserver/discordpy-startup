618770ef06c92081db0f6b53b52edbbe5d0a5e43
kenkenserver <51988985+kenkenserver@users.noreply.github.com>
2020-06-30 22:02:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,6 +17,8 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.command()
+async def fill(ctx):
 embed.add_field(name="サーバー",value="[招待コード](https://discord)")
 
 
