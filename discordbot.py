1affb2ce561e05c22110fee4162a157a5dc35e89
kenkenserver <51988985+kenkenserver@users.noreply.github.com>
2020-06-30 21:34:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,7 +9,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
-    error_msg = 'test'.join(traceback.TracebackException.from_exception(orig_error).format())
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
 
